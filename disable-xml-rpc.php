28a2999231a90bf2bfc1df9325dbfae3b5f0ea27
Jesse Nickles <jesse@littlebizzy.com>
2025-03-22 05:26:56 +0700

    Update disable-xml-rpc.php

--- disable-xml-rpc.php
+++ disable-xml-rpc.php
@@ -28,47 +28,47 @@ add_filter( 'gu_override_dot_org', function( $overrides ) {
 }, 999 );
 
 // disable xmlrpc completely
-add_filter('xmlrpc_enabled', '__return_false');
+add_filter( 'xmlrpc_enabled', '__return_false' );
 
 // immediately terminate any xmlrpc requests
-add_action('xmlrpc_call', function() {
-    header('HTTP/1.1 403 Forbidden');
-    exit;
-});
-
-// remove rsd link in head that exposes xmlrpc.php
-remove_action('wp_head', 'rsd_link');
-
-// disable pingbacks and trackbacks by default
-add_filter('pre_option_default_ping_status', '__return_zero');
-add_filter('pre_option_default_pingback_flag', '__return_zero');
-
-// Hide pingback and trackback options on the Discussion settings page
-add_action('admin_enqueue_scripts', function ($hook) {
-    if ($hook === 'options-discussion.php') {
-        wp_add_inline_style('dashboard', '
-            .form-table td label[for="default_pingback_flag"], 
-            .form-table td label[for="default_pingback_flag"] + br, 
-            .form-table td label[for="default_ping_status"], 
-            .form-table td label[for="default_ping_status"] + br {
-                display: none;
-            }
-        ');
-    }
-});
-
-// remove pingback URL from headers
-add_filter('wp_headers', function ($headers) {
-    unset($headers['X-Pingback']);
-    return $headers;
-});
+add_action( 'xmlrpc_call', function() {
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit;
+} );
 
 // block direct access to xmlrpc.php
-add_action('init', function () {
-    if (isset($_SERVER['SCRIPT_FILENAME']) && basename($_SERVER['SCRIPT_FILENAME']) === 'xmlrpc.php') {
-        header('HTTP/1.1 403 Forbidden');
-        exit;
-    }
-}, 1);
+add_action( 'init', function() {
+	if ( isset( $_SERVER['SCRIPT_FILENAME'] ) && basename( $_SERVER['SCRIPT_FILENAME'] ) === 'xmlrpc.php' ) {
+		header( 'HTTP/1.1 403 Forbidden' );
+		exit;
+	}
+}, 1 );
+
+// remove rsd link in head that exposes xmlrpc.php
+remove_action( 'wp_head', 'rsd_link' );
+
+// remove pingback url from headers
+add_filter( 'wp_headers', function( $headers ) {
+	unset( $headers['X-Pingback'] );
+	return $headers;
+} );
+
+// disable pingbacks and trackbacks by default
+add_filter( 'pre_option_default_ping_status', '__return_zero' );
+add_filter( 'pre_option_default_pingback_flag', '__return_zero' );
+
+// hide pingback and trackback options on the discussion settings page
+add_action( 'admin_enqueue_scripts', function( $hook ) {
+	if ( $hook === 'options-discussion.php' ) {
+		wp_add_inline_style( 'dashboard', '
+			.form-table td label[for="default_pingback_flag"],
+			.form-table td label[for="default_pingback_flag"] + br,
+			.form-table td label[for="default_ping_status"],
+			.form-table td label[for="default_ping_status"] + br {
+				display: none;
+			}
+		' );
+	}
+} );
 
 // Ref: ChatGPT
